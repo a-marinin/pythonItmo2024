51c83a5aa148d0a9c786d370d95adf595c502345
amarinin <aleksandrqa1@gmail.com>
2024-07-14 20:40:20 +0300

    2nd homework - class work is done

--- home_work/home_work_02.py
+++ home_work/home_work_02.py
@@ -17,14 +17,15 @@ print('type(x): ', type(x))
 print()
 
 # Задачи № 3 и 4
-month_num = 8
+month_num = 99
 month_list = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
               'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
 if month_num in range(1, 13):  # Если число входит в диапозон от 1 до 12
     # Выводим в консоль номер месяца и его название
     print(f"{month_num}-й месяц года - это {month_list[month_num-1]}")
-elif month_num not in range(1, 13) and isinstance(month_num, int):  # Если чисто не входит в диапозон от 1 до 12
-    print(f"{month_num} - это не похоже на номер месяца")
-elif not isinstance(month_num, int):  # Если чисто не является целым числом (int)
-    print(f"{month_num} - это не целое число")
-
+elif month_num not in range(1, 13) and isinstance(month_num, int):  # Если целое чисто не входит в диапозон от 1 до 12
+    print(f"{month_num} - это не похоже на номер месяца")  # Выводим сообещние в консоль
+    raise Exception('Данное целое число не входит в диапозон от 1 до 12')  # Выводим кастомное исключение
+elif not isinstance(month_num, int):  # Если введённое значение не является целым числом (int)
+    print(f"{month_num} - это не целое число")  # Выводим сообещние в консоль
+    raise Exception('Данное число не является целым')  # Выводим кастомное исключение
