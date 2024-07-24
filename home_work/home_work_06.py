630ed24ea024374ccafbbf2a517d61197be26c79
amarinin <aleksandrqa1@gmail.com>
2024-07-24 21:41:06 +0300

    6th homework - 2nd task is done

--- home_work/home_work_06.py
+++ home_work/home_work_06.py
@@ -21,12 +21,49 @@ class Rectangle:
 triangle1 = Rectangle(side_1=1, side_2=2)
 triangle2 = Rectangle(side_1=7, side_2=8)
 
-triangle1.calculate_perimeter()  # Периметр прямоугольника (со сторонами 1 и 2) равен: 6.
-triangle2.calculate_perimeter()  # Периметр прямоугольника (со сторонами 7 и 8) равен: 30.
-
-triangle1.calculate_area()  # Площадь прямоугольника (со сторонами 1 и 2) равна: 2.
-triangle2.calculate_area()  # Площадь прямоугольника (со сторонами 7 и 8) равна: 56.
+# triangle1.calculate_perimeter()  # Периметр прямоугольника (со сторонами 1 и 2) равен: 6.
+# triangle2.calculate_perimeter()  # Периметр прямоугольника (со сторонами 7 и 8) равен: 30.
+#
+# triangle1.calculate_area()  # Площадь прямоугольника (со сторонами 1 и 2) равна: 2.
+# triangle2.calculate_area()  # Площадь прямоугольника (со сторонами 7 и 8) равна: 56.
 
 
 # Задача № 2
+class Book:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self, title, author, checked_out=False):
+        self.title = title
+        self.author = author
+        self.checked_out = checked_out
+
+    def check_out(self):
+        if not self.checked_out:
+            print(f'{self.author} {self.title} - Выдаем книгу абоненту.')
+            self.checked_out = True
+        else:
+            print(f'{self.author} {self.title} - Книга находится у абонента.')
+
+    def check_in(self):
+        if not self.checked_out:
+            print(f'{self.author} {self.title} - Книга в наличии.')
+        else:
+            print(f'{self.author} {self.title} - Принимаем книгу в библиотеку.')
+            self.checked_out = False
+
+
+book1 = Book(title='Евгений Онегин', author='А. С. Пушкин')
+book2 = Book(title='Братья Карамазовы', author='Ф. М. Достоевский')
+
+print(id(book1), id(book2))  # 4405531216 4405531216
+book1.check_out()  # Ф. М. Достоевский Братья Карамазовы - Выдаем книгу абоненту.
+book1.check_out()  # Ф. М. Достоевский Братья Карамазовы - Книга находится у абонента.
+book1.check_in()  # Ф. М. Достоевский Братья Карамазовы - Принимаем книгу в библиотеку.
+book1.check_in()  # Ф. М. Достоевский Братья Карамазовы - Книга в наличии.
+
 # Задача № 3
